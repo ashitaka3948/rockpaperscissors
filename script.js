3963815de2aac48310d3294ef3517f35b67690de
ashitaka3948 <[email protected>
2024-09-21 02:17:57 +0800

    use DOM methods

--- script.js
+++ script.js
@@ -23,77 +23,73 @@ const rockButton = document.querySelector("#rockButton");
 const paperButton = document.querySelector("#paperButton");
 const scissorsButton = document.querySelector("#scissorsButton");
 */
-const computerSelection = getComputerChoice();
-let humanSelection;
-
+// const computerSelection = getComputerChoice();
+// let humanSelection = 0;
 const buttons = document.querySelectorAll("button");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    humanSelection = button.id;
-  });
-});
-
-/*
-rockButton.addEventListener("click", function (e) {
-  humanSelection = "rock";
-});
-paperButton.addEventListener("click", function (e) {
-  humanSelection = "paper";
-});
-scissorsButton.addEventListener("click", function (e) {
-  humanSelection = "scissors";
-})
-*/
+const score = document.querySelector("#results");
+const comments = document.querySelector("#comments");
+const winner = document.querySelector("#winner");
 
 let humanScore = 0;
 let computerScore = 0;
 
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const humanSelection = button.id;
+    console.log(humanSelection);
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  });
+});
+
 function playRound(humanChoice, computerChoice) {
-  console.log(humanChoice.toLowerCase());
-  if (humanChoice.toLowerCase === computerChoice) {
-    console.log("It's a draw! Fucking boring.");
+  if (humanChoice === computerChoice) {
+    comments.textContent = "It's a draw! Fucking boring.";
     displayScore();
-  } else if (humanChoice.toLowerCase() === "rock" && computerChoice === "paper") {
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
     computerScore++;
-    console.log("You suck! Paper fucks rock.");
+    comments.textContent = "You suck! Paper fucks rock.";
     displayScore();
-  } else if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
     humanScore++;
-    console.log("You win cuz rock fucks up the scissors.");
+    comments.textContent = "You win cuz rock fucks up the scissors.";
     displayScore();
-  } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") {
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
     humanScore++;
-    console.log("You win cuz paper fucks rock.");
+    comments.textContent = "You win cuz paper fucks rock.";
     displayScore();
-  } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "scissors") {
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
     computerScore++;
-    console.log("You suck!  Scissors fuck you your shitty paper!");
+    comments.textContent = "You suck!  Scissors fuck you your shitty paper!";
     displayScore();
-  } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
     humanScore++;
-    console.log("You win cuz scissors fuck up the paper.");
+    comments.textContent = "You win cuz scissors fuck up the paper.";
     displayScore();
-  } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "rock") {
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
     computerScore++;
-    console.log("You fucking lost cuz rock fucks up your scissors!");
+    comments.textContent = "You fucking lost cuz rock fucks up your scissors!";
     displayScore();
   } else {
     computerScore++;
-    console.log("Da fuck did u enter bitch? You lose this round");
+    comments.textContent = "Da fuck did u enter bitch? You lose this round";
     displayScore();
   }
 }
 
 function displayScore() {
-  console.log("Current score: Human " + humanScore + "  Computer " + computerScore);
+  score.textContent = `Current score: Human ${humanScore}  Computer ${computerScore}`;
+  if (humanScore === 5) {
+    winner.textContent = "You won.  You are da shit!";
+  } else if (computerScore === 5) {
+    winner.textContent = "You lost.  You fucking suck.";
+  }
 }
-
-/* 
+/*
 function playGame() {
 
   for (let i = 1; i <= 5; i++) {
-    const humanSelection = getHumanChoice();
+  //  const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection);
   }
